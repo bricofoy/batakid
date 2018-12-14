fbc4d40a716292f8df0775fcabbd21f44d11e9c1
Yann Jautard <bricofoy@free.fr>
2018-12-15 00:01:55 +0100

    ajout de quelques fonctions utilitaires

--- batakid.ino
+++ batakid.ino
@@ -7,16 +7,41 @@
 #include "yasm.h"
 #include "btn.h"
 
+#define ON HIGH
+#define OFF LOW
 #define BTN_NBR 10
-const byte ledPin[BTN_NBR]={13,12,11,10,9,8,7,6,5,4};
-const byte btnPin[BTN_NBR]={};
+const byte LedPin[BTN_NBR]={13,12,11,10,9,8,7,6,5,4};
+const byte BtnPin[BTN_NBR]={};
 
-bool ledState[BTN_NBR]={0};
+bool LedState[BTN_NBR]={0};
 
 void setup
 {
 	for (byte i=0; i<BTN_NBR; i++) {
-		pinMode(ledPin[i],OUTPUT);
-		pinMode(btnPin[i],INPUT_PULLUP);
+		pinMode(LedPin[i],OUTPUT);
+		pinMode(BtnPin[i],INPUT_PULLUP);
 	}
+	
+	chenillard(50);
+	toutesLeds(ON);
+	delay(50);
+	toutesLeds(OFF);
+}
+
+void affiche()
+{ 
+	for (byte i=0; i<BTN_NBR; i++)
+		digitalWrite(LedPin[i], LedState[i]);
+}
+
+void toutesLeds(bool aEtat)
+{
+	for (byte i=0; i<BTN_NBR; i++)
+		LedState[i]=aEtat;
+	affiche();
+}
+
+void loop
+{
+	
 }
