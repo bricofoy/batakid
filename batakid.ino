a988b2b7e16f8fc0819e574b9bf0ae4d2206a6c1
Yann Jautard <bricofoy@free.fr>
2018-12-20 01:59:49 +0100

    ajout de la fonction chenillard()
    et donc de tout ce qui va avec pour que ça marche

--- batakid.ino
+++ batakid.ino
@@ -7,17 +7,56 @@
 #include "yasm.h"
 #include "btn.h"
 
-#define ON HIGH
-#define OFF LOW
+#define ON true
+#define OFF false
 #define BTN_NBR 10
-const byte LedPin[BTN_NBR]={13,12,11,10,9,8,7,6,5,4};
-const byte BtnPin[BTN_NBR]={};
+const int8_t LedPin[BTN_NBR]={13,12,11,10,9,8,7,6,5,4};
+const int8_t BtnPin[BTN_NBR]={};
 
 bool LedState[BTN_NBR]={0};
 
+YASM ChenSM;
+unsigned long ChenDelai;
+int8_t ChenNbr;
+void chenillard(unsigned long aDelai, int8_t aNombre=1, bool aAttendreFin=true)
+{
+	ChenDelai = aDelai;
+	ChenNbr = aNombre;
+	ChenSM.next(chen);
+	if( aAttendreFin )
+		while( ChenSM.run() );
+}
+
+void chen()
+{
+	static int8_t position;
+	
+	if( ChenSM.isFirstRun() )
+		position = 0;
+
+	if( position = (BTN_NBR+ChenNbr-1) ) {
+		//si on est arrivé au bout de la chenille, on stoppe le chenillard
+		ChenSM.stop();
+		//et on remet le dernière led utilisée dans son état initial
+		digitalWrite(LedPin[BTN_NBR-1], !digitalRead(LedPin[BTN_NBR-1]));
+		//puis on quitte avant de rallumer un truc
+		return;
+	}
+	
+	//basculement de l'état de la led courante
+	if( position<BTN_NBR )
+		digitalWrite(LedPin[position], !digitalRead(LedPin[position]));
+	
+	//extinction de la dernière led de la chenille
+	int8_t condition = (position-ChenNbr);
+	if( (condition) > -1)
+		digitalWrite(LedPin[condition], !digitalRead(LedPin[condition]));
+}
+
+
 void setup
 {
-	for (byte i=0; i<BTN_NBR; i++) {
+	for(int8_t i=0; i<BTN_NBR; i++) {
 		pinMode(LedPin[i],OUTPUT);
 		pinMode(BtnPin[i],INPUT_PULLUP);
 	}
@@ -28,22 +67,18 @@ void setup
 	toutesLeds(OFF);
 }
 
-void chenillard(unsigned long aDelai, byte aNombre=1, bool aAttendreFin=true)
-{
-	//faire une classe dérivée de yasm pour rendre une variable de délai accessible
-	//ensuite une SM de cette classe pout faire le chenillard
-}
+
 
 
 void affiche()
 { 
-	for (byte i=0; i<BTN_NBR; i++)
+	for(int8_t i=0; i<BTN_NBR; i++)
 		digitalWrite(LedPin[i], LedState[i]);
 }
 
 void toutesLeds(bool aEtat)
 {
-	for (byte i=0; i<BTN_NBR; i++)
+	for(int8_t i=0; i<BTN_NBR; i++)
 		LedState[i]=aEtat;
 	affiche();
 }
