2a99ccbc362328f72016607cf4df9fbdacdc47ee
Yann Jautard <bricofoy@free.fr>
2018-12-20 02:14:44 +0100

    nettoyage et réorganisation du code

--- batakid.ino
+++ batakid.ino
@@ -7,14 +7,28 @@
 #include "yasm.h"
 #include "btn.h"
 
+/****définitions relatives aux leds et aux boutons************************************/
 #define ON true
 #define OFF false
 #define BTN_NBR 10
 const int8_t LedPin[BTN_NBR]={13,12,11,10,9,8,7,6,5,4};
 const int8_t BtnPin[BTN_NBR]={};
-
 bool LedState[BTN_NBR]={0};
 
+void affiche()
+{ 
+	for(int8_t i=0; i<BTN_NBR; i++)
+		digitalWrite(LedPin[i], LedState[i]);
+}
+
+void toutesLeds(bool aEtat)
+{
+	for(int8_t i=0; i<BTN_NBR; i++)
+		LedState[i]=aEtat;
+	affiche();
+}
+
+/****** fonction de chenillard ******************************************************/
 YASM ChenSM;
 unsigned long ChenDelai;
 int8_t ChenNbr;
@@ -34,7 +48,7 @@ void chen()
 	if( ChenSM.isFirstRun() )
 		position = 0;
 
-	if( position = (BTN_NBR+ChenNbr-1) ) {
+	if( position = (BTN_NBR-1+ChenNbr) ) {
 		//si on est arrivé au bout de la chenille, on stoppe le chenillard
 		ChenSM.stop();
 		//et on remet le dernière led utilisée dans son état initial
@@ -48,42 +62,31 @@ void chen()
 		digitalWrite(LedPin[position], !digitalRead(LedPin[position]));
 	
 	//extinction de la dernière led de la chenille
-	int8_t condition = (position-ChenNbr);
-	if( (condition) > -1)
-		digitalWrite(LedPin[condition], !digitalRead(LedPin[condition]));
+	int8_t temp = (position-ChenNbr);
+	if( (temp) > -1)
+		digitalWrite(LedPin[temp], !digitalRead(LedPin[temp]));
 }
 
 
-void setup
+
+
+
+
+
+void setup()
 {
 	for(int8_t i=0; i<BTN_NBR; i++) {
 		pinMode(LedPin[i],OUTPUT);
 		pinMode(BtnPin[i],INPUT_PULLUP);
 	}
 	
-	chenillard(50);
+	chenillard(50, 3, true);
 	toutesLeds(ON);
-	delay(50);
+	chenillard(50, 4, true);
 	toutesLeds(OFF);
 }
 
-
-
-
-void affiche()
-{ 
-	for(int8_t i=0; i<BTN_NBR; i++)
-		digitalWrite(LedPin[i], LedState[i]);
-}
-
-void toutesLeds(bool aEtat)
-{
-	for(int8_t i=0; i<BTN_NBR; i++)
-		LedState[i]=aEtat;
-	affiche();
-}
-
-void loop
+void loop()
 {
 	
 }
